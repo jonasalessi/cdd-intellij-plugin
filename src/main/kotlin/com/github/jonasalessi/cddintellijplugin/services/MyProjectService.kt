faad27c00bfad710910fed4c528c7450a7440315
GitHub Action <action@github.com>
2026-01-04 21:10:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/jonasalessi/cddintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jonasalessi/cddintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.jonasalessi.cddintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jonasalessi.cddintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
